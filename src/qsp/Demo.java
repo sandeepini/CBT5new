50c5c745d0392bf700ee9e8734fdb217764bc290
CBT <sandeepkondaiah@gmail.com>
2018-02-04 17:12:53 +0530

    hi

--- src/qsp/Demo.java
+++ src/qsp/Demo.java
@@ -2,7 +2,6 @@ package qsp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Demo {
@@ -14,13 +13,8 @@ public class Demo {
 //		WebDriver driver=new RemoteWebDriver(r,d);
 	//driver.quit();
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		Reporter.log("open the browser",true);
 		WebDriver driver=new ChromeDriver();
-		Reporter.log("enter the url",true);
-		
 		driver.get("http://www.google.com");
-		Reporter.log("closing",true);
-		
 			driver.quit();
 		
 	}
